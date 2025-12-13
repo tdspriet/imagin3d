931f8e5de88457020f16e3414988a9e06f597840
Thibaud <thibaud.despriet@gmail.com>
2025-12-13 16:08:35 +0000

    🐛 BACKEND: fix run bug

--- backend/run.py
+++ backend/run.py
@@ -1,4 +1,8 @@
+import sys
+from pathlib import Path
 import uvicorn
+
+sys.path.insert(0, str(Path(__file__).parent.parent))
 
 if __name__ == "__main__":
     uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
